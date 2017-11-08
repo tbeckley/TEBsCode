bf6b5359e50e8c316454edbe8f8a477a5d1e340a
Thomas Beckley <tbeckley99@gmail.com>
2017-11-07 21:48:23 -0500

    Added stupid headers

--- MyCode/CPlusPlus/Lab08/Lab0802/main_matrixmath.cpp
+++ MyCode/CPlusPlus/Lab08/Lab0802/main_matrixmath.cpp
@@ -1,3 +1,20 @@
+//*********************************************
+//  Student Name:  Thomas Beckley
+//  Student Number: 20703455
+//
+//  SYDE121    Lab: 08 Assignment: 01
+//
+//  Filename: main_matrixmath.h
+//  Date submitted: 2017 - 11 - 07
+//
+//  I hereby declare that this code, submitted
+//  for credit for the course SYDE121, is a product
+//  of my own efforts.  This coded solution has
+//  not been plagiarized from other sources and
+//  has not been knowingly plagiarized by others.
+//*********************************************
+
+//Function of program: Read matrices from file, multiply matrices, and write end result to output file
 #include <iostream>
 #include <fstream>
 #include "matrixmath.h"
--- MyCode/CPlusPlus/Lab08/Lab0802/matrixmath.cpp
+++ MyCode/CPlusPlus/Lab08/Lab0802/matrixmath.cpp
@@ -1,3 +1,18 @@
+//*********************************************
+//  Student Name:  Thomas Beckley
+//  Student Number: 20703455
+//
+//  SYDE121    Lab: 08 Assignment: 01
+//
+//  Filename: matrixmath.cpp
+//  Date submitted: 2017 - 11 - 07
+//
+//  I hereby declare that this code, submitted
+//  for credit for the course SYDE121, is a product
+//  of my own efforts.  This coded solution has
+//  not been plagiarized from other sources and
+//  has not been knowingly plagiarized by others.
+//*********************************************
 #include<iostream>
 #include<fstream>
 #include "matrixmath.h"

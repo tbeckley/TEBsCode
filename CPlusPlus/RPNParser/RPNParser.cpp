22f47eea62b884adaa4434a11e0c7c3061448046
Thomas Beckley <thomas.beckley@intelex.com>
2018-01-19 09:41:56 -0500

    C++ and Java Things

--- CPlusPlus/RPNParser/RPNParser.cpp
+++ CPlusPlus/RPNParser/RPNParser.cpp
@@ -1,19 +1,11 @@
 #include<iostream>
-#include "Stack.cpp"
+#include<stack>
+
+
 using namespace std;
 
 int main(void)
 {
     system ("clear");
-    cout << "D O N G!" << endl;
-    Stack myStack;
-    myStack.push(4);
-    myStack.push(12);
-    myStack.push(2351);
-    myStack.push(106.4);
-
-    cout << myStack.pop() << endl;
-    cout << myStack.pop() << endl;
-    cout << myStack.pop() << endl;
-    cout << myStack.pop() << endl;
+    cout << "Welcome to my RPN Parser" << endl;
 }
---
+++ Java/RPNParser/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class HelloWorld
+{
+    public static void Main(string [] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
